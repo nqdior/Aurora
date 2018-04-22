99515eb5904ae35748050dcc8767b75bcd4296c3
Delight <loughingcat@gmail.com>
2018-04-22 22:44:22 +0900

    VisualStudioの自動補完削除

--- Aurora.Viewer/BaseForm.cs
+++ Aurora.Viewer/BaseForm.cs
@@ -4,24 +4,11 @@ namespace Aurora.Viewer
 {
     public partial class BaseForm : Form
     {
-
         public BaseForm() 
         {
             ControlBox = false;            
         }
-
+        
         public void MoveForm(MouseEventArgs e) => FormManager.ConvertMessageMove(e, Handle);
-
-        private void InitializeComponent()
-        {
-            this.SuspendLayout();
-            // 
-            // BaseForm
-            // 
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Name = "BaseForm";
-            this.ResumeLayout(false);
-
-        }
     }
 }
