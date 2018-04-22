290f2c79b1642cd03b111b09af2f7c29b0e6c707
Delight <loughingcat@gmail.com>
2018-04-22 16:56:32 +0900

    フォームつくりはじめたよ

---
+++ Aurora.Viewer/BaseForm.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows.Forms;
+
+namespace Aurora.Viewer
+{
+    public partial class BaseForm : Form
+    {
+
+        public BaseForm() 
+        {
+            ControlBox = false;            
+        }
+
+        public void MoveForm(MouseEventArgs e) => FormManager.ConvertMessageMove(e, Handle);
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // BaseForm
+            // 
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Name = "BaseForm";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ Aurora.Viewer/Form1.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace Aurora.Viewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formBar1 = new Aurora.Viewer.FormBar();
+            this.SuspendLayout();
+            // 
+            // formBar1
+            // 
+            this.formBar1.BackColor = System.Drawing.Color.White;
+            this.formBar1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.formBar1.ForeColor = System.Drawing.Color.White;
+            this.formBar1.isController = false;
+            this.formBar1.Location = new System.Drawing.Point(0, 0);
+            this.formBar1.Name = "formBar1";
+            this.formBar1.Size = new System.Drawing.Size(549, 28);
+            this.formBar1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(549, 284);
+            this.Controls.Add(this.formBar1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FormBar formBar1;
+    }
+}
