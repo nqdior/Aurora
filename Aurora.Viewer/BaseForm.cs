1bbc3483f0eb58e3df8881c01e5b3151c665581a
Delight <loughingcat@gmail.com>
2018-04-24 03:10:28 +0900

    BaseFormCreated.

--- Aurora.Viewer/BaseForm.cs
+++ Aurora.Viewer/BaseForm.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing;
 
 namespace Aurora.Viewer
 {
@@ -6,9 +8,34 @@ namespace Aurora.Viewer
     {
         public BaseForm() 
         {
-            ControlBox = false;            
+            ControlBox = false;
+            InitializeComponent();        
         }
-        
-        public void MoveForm(MouseEventArgs e) => FormManager.ConvertMessageMove(e, Handle);
+
+        public void InitializeComponent()
+        {
+            var resource = new ComponentResourceManager(typeof(BaseForm));
+            var Button_Min = new ToolStripMenuItem((Image)resource.GetObject("Min"))
+            {
+                Alignment = ToolStripItemAlignment.Right
+            };
+            var Button_Max = new ToolStripMenuItem((Image)resource.GetObject("Max"))
+            {
+                Alignment = ToolStripItemAlignment.Right
+            };
+            var Button_Close = new ToolStripMenuItem((Image)resource.GetObject("Close"))
+            {
+                Alignment = ToolStripItemAlignment.Right
+            };
+            var FormBar = new MenuStrip();
+            FormBar.Items.AddRange(new ToolStripItem[] { Button_Close, Button_Max, Button_Min });
+            Controls.Add(FormBar);
+
+            Button_Min.Click += (sender, e) => WindowState = FormWindowState.Minimized;
+            Button_Max.Click += (sender, e) => WindowState = WindowState == FormWindowState.Maximized ? FormWindowState.Normal : FormWindowState.Maximized;
+            Button_Close.Click += (sender, e) => Close();
+            FormBar.MouseDown += (sender, e) => FormManager.ConvertMessageMove(e, Handle);
+            FormBar.MouseDoubleClick += (sender, e) => Button_Max.PerformClick();
+        }
     }
 }
--- Aurora.Viewer/Program.cs
+++ Aurora.Viewer/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Aurora.Viewer
@@ -9,14 +6,14 @@ namespace Aurora.Viewer
     static class Program
     {
         /// <summary>
-        /// アプリケーションのメイン エントリ ポイントです。
+        /// test
         /// </summary>
         [STAThread]
         static void Main()
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new BaseForm());
         }
     }
 }
