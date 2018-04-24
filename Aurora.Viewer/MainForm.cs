865fe124004cfc714c6630dd6c6a69f9507626b8
Delight <loughingcat@gmail.com>
2018-04-25 01:43:10 +0900

    aa

---
+++ Aurora.Viewer/MainForm.cs
@@ -0,0 +1,27 @@
+﻿using Aurora.Data;
+using System;
+using System.Windows.Forms;
+
+namespace Aurora.Viewer
+{
+    public partial class MainForm : BaseForm
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            var Select_Server = new ToolStripComboBox()
+            {
+                Alignment = ToolStripItemAlignment.Right,
+                DropDownStyle = ComboBoxStyle.DropDownList,
+                FlatStyle = FlatStyle.Flat
+            };
+            Select_Server.Items.Add("SQLServer");
+            TitleBar.Items.Add(Select_Server);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
